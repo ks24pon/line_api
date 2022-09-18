2af310e4a2b935f2eab288e7ffc522028254c323
ks2424 <pom123re0624@gmail.com>
2022-09-18 12:21:33 +0900

    line

--- Docker/app/controllers/line_api_controller.rb
+++ Docker/app/controllers/line_api_controller.rb
@@ -1,5 +1,15 @@
 class LineApiController < ApplicationController
-  
+  #line_apt
   def back
   end
+
+private
+  #line_api作成
+  def clinet
+    @clinet ||= Line::Bot::Clinet.new { |config|
+      config.channel_secret = ENV["2cdca6acf10755fb6733ca8a3cc68e22"]
+      config.channel_token = ENV["bDy6XOsVcj95iADdaF6lA4s+xcZiO7uoF6RzpRbxcNnm38LXCHQN8aloDIxvDczTy/ilWYZdEc0InrHepLZmB/AssEeALatBwyWpkvuYzbSZkCTYH/h8VWBvtFtXaZO/3EE6PUr4akvjBQgagLztHwdB04t89/1O/w1cDnyilFU="]
+    }
+  end
+
 end
